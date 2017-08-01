b63d38dd3357c3945a85dc67b33b1198edc7e692
Duc Dang <your@email.com>
2017-08-01 15:38:46 -0500

    yo

---
+++ hash.rb
@@ -0,0 +1,8 @@
+class User
+  attr_accessor :first_name, :last_name
+  def initialize(f_name, l_name)
+    @first_name = f_name
+    @last_name = l_name
+  end
+end
+steph = User.new("Stephen", "Curry")
