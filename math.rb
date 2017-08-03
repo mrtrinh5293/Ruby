0b8e5ced9f8298db472e1590725ca9561873fe54
Duc Dang <your@email.com>
2017-08-03 13:30:46 -0500

    user

--- math.rb
+++ math.rb
@@ -1,22 +1,7 @@
-class MathDojo
-  attr_reader :answer
+a = "dsd"
 
-  def initialize
-    @answer = 0
-  end
-
-  def add(*numbers)
-    @answer += numbers.flatten.reduce(0, :+)
-    self
-  end
-
-  def subtract(*numbers)
-    @answer -= numbers.flatten.reduce(0, :+)
-    self
-  end
+def add
+    puts "dsds #{a}"
 end
 
-challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).answer
-challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).answer
-puts challenge1
-puts challenge2
+add
